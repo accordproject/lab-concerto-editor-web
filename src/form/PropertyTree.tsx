9656054ca0ee50544d3b298c238a1efa57c07ce7
Ayman161803 <mohammedaymannawaz@gmail.com>
2022-12-14 18:25:39 +0530

    feat(form): prevent multiselect on property tree
    Signed-off-by: Ayman161803 <mohammedaymannawaz@gmail.com>

--- src/form/PropertyTree.tsx
+++ src/form/PropertyTree.tsx
@@ -1,48 +1,51 @@
 import Tree, { NodeId, NodeList } from '@naisutech/react-tree'
 import { IConceptDeclaration, IEnumDeclaration } from '../metamodel/concerto.metamodel';
 
+import { TreeItem, TreeView } from '@mui/lab';
+import { ExpandMore as ExpandMoreIcon, ChevronRight as ChevronRightIcon } from '@mui/icons-material'
+
 import useStore from '../store';
+import { SyntheticEvent } from 'react';
 
 function PropertyTree() {
   const models = useStore(state => state.models);
   const editorItemSelected = useStore(state => state.editorItemSelected);
 
-  function onTreeSelect(id: Array<NodeId>) {
-    if(id && id.length > 0) {
-      editorItemSelected(id[id.length-1].toString());
-    }
+  const buildTree = () => {
+    return Object.values(models).map(modelEntry => {
+      return (
+        <TreeItem 
+          nodeId={modelEntry.model.namespace} 
+          label={modelEntry.model.namespace}>
+          {modelEntry.model.declarations?.map(decl => {
+            return (
+              <TreeItem 
+                nodeId={`${modelEntry.model.namespace}#${decl.name}`} 
+                label={decl.name}>
+                {(decl as IEnumDeclaration | IConceptDeclaration).properties.map(prop => {
+                  return <TreeItem nodeId={`${modelEntry.model.namespace}#${decl.name}.${prop.name}`} label={prop.name}></TreeItem>
+                })}
+              </TreeItem>
+            )
+          })}
+        </TreeItem>
+      )
+    });
   }
 
-  const data: NodeList = [];
-  Object.values(models).forEach(modelEntry => {
-    // push namespace, which is a root items and parent to declarations
-    data.push({
-      id: modelEntry.model.namespace,
-      parentId: null,
-      label: modelEntry.model.namespace
-    });
-    // push declarations, which has a namespace as a parent, and is a parent to properties
-    modelEntry.model.declarations?.forEach(decl => {
-      data.push({
-        id: `${modelEntry.model.namespace}#${decl.name}`,
-        parentId: modelEntry.model.namespace,
-        label: decl.name,
-        items: (decl as IEnumDeclaration | IConceptDeclaration).properties.map(prop => {
-          return {
-            id: `${modelEntry.model.namespace}#${decl.name}.${prop.name}`,
-            parentId: `${modelEntry.model.namespace}#${decl.name}`,
-            label: prop.name
-          }
-        })
-      })
-    })
-  });
-
   return (
-    <Tree nodes={data} 
-      theme='light'
-      onSelect={onTreeSelect}
-    />
+    <>
+      <TreeView
+        aria-label="file system navigator"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+        onNodeSelect={(e: SyntheticEvent, nodeIds: string) => editorItemSelected(nodeIds)}
+      >
+        {buildTree()}
+      </TreeView>
+    </>
+    
   )
 }
 
